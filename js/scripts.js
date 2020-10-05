a4f5bbaf77aff6bef957db97b4d4ef1ae4f6981d
thyezoliveira <thyezoliveira@gmail.com>
2020-10-05 20:03:47 -0300

    About
    Crated an about section. :)

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+let aboutBtn = document.querySelector("#aboutBtn");
+let menu = document.querySelector("#menu");
+let closeBtn = document.querySelector("#closeBtn");
+let main = document.querySelector("main");/* 
+let footer = document.querySelector("footer"); */
+
+/* window.addEventListener('scroll', rolagem); */
+aboutBtn.addEventListener('click', alternarSobre);
+closeBtn.addEventListener('click', alternarSobre);
+
+function alternarSobre(){
+    closeBtn.classList.toggle('hidden');
+    menu.classList.toggle('hidden');
+    main.classList.toggle('hidden');
+}
+
+/* function rolagem(){
+    let posicao = main.offset();
+    console.log(posicao.top);
+} */
