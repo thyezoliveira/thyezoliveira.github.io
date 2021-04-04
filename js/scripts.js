bdc6b22fc0a5d9724637bd03646f0e8305df3ae5
thyezoliveira <thyezoliveira@gmail.com>
2021-04-04 10:57:02 -0300

    html1.0.4

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
 const avatar = document.querySelector(".bio_avatar");
+let device = "";
+setInterval(()=>{
+    device = (window.innerWidth > 700) ? "desktop" : "handheld";
+    if(device == "handheld"){
+        avatar.addEventListener('mouseover', ()=>{
+            avatar.style.animation = "grow2x 3s ease-in-out forwards";
+        })
+    
+        avatar.addEventListener('mouseout', ()=>{
+            //animation: jump 5s ease-in-out infinite;
+            avatar.style.animation = "jump 5s ease-in-out infinite";
+        })
+    }
+}, 50)
+
+
