02bce4910c1a70c75b62059b602f82cfb86ada53
thyezoliveira <thyezoliveira@gmail.com>
2021-04-04 15:25:38 -0300

    final1.1.0

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ projects.forEach((project, i) => {
         <p class="projects_article_description">${project.descricao}</p>
         <div class="projects_article_footer">
             <p class="projects_article_footer_date">${project.data}</p>
-            <a href="${project.link}" id="projectBtn${i}" class="projects_article_footer_a"><img src="./assets/iconLink.svg"
+            <a href="${project.link}" target="_blank" id="projectBtn${i}" class="projects_article_footer_a"><img src="./assets/iconLink.svg"
                     alt=""></a>
         </div>
     </article>
