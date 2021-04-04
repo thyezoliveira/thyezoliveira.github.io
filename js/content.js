ad09b8260433e41b65f05e48c22eb674a48c53a6
thyezoliveira <thyezoliveira@gmail.com>
2021-04-04 15:20:32 -0300

    js1.0.1

---
+++ js/content.js
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+        titulo: "Card Game - Super Trunfo",
+        imagem: "https://github.com/thyezoliveira/cardGame_SuperTrunfo/raw/main/assets/captura1.png",
+        imagemAlt: "Captura de tela do jogo Super Trunfo, feito por Thyéz Oliveira.",
+        descricao: "Um mini jogo de cartas, no estilo Super Trunfo. Desenvolvido em Javascript.",
+        data: "01/04/2021",
+        link: "https://github.com/thyezoliveira/cardGame_SuperTrunfo"
+    }
+]
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,46 @@
-const avatar = document.querySelector(".bio_avatar");
-let device = "";
-setInterval(()=>{
-    device = (window.innerWidth > 700) ? "desktop" : "handheld";
-    if(device == "handheld"){
-        avatar.addEventListener('mouseover', ()=>{
-            avatar.style.animation = "grow2x 3s ease-in-out forwards";
-        })
-    
-        avatar.addEventListener('mouseout', ()=>{
-            //animation: jump 5s ease-in-out infinite;
-            avatar.style.animation = "jump 5s ease-in-out infinite";
-        })
-    }
-}, 50)
+import { projects } from "./content.js";
+
+let shields = document.querySelectorAll(".bio_skills_holder_skill");
+let projectsHolder = document.querySelector(".projects");
+let allProjects = "";
+
+projects.forEach((project, i) => {
+    let template = `
+    <article class="projects_article">
+        <header class="projects_article_header">
+            <h3>${project.titulo}</h3>
+        </header>
+        <img src="${project.imagem}" alt="${project.imagemAlt}" class="projects_article_thumb">
+        <p class="projects_article_description">${project.descricao}</p>
+        <div class="projects_article_footer">
+            <p class="projects_article_footer_date">${project.data}</p>
+            <a href="${project.link}" id="projectBtn${i}" class="projects_article_footer_a"><img src="./assets/iconLink.svg"
+                    alt=""></a>
+        </div>
+    </article>
+    `;
+    allProjects += template;
+})
+
+projectsHolder.innerHTML = allProjects;
 
 
+function activate(){
+    shields.forEach(shield => {
+        shield.addEventListener("mouseover", (event)=>{
+            let icon = event.target;
+            if(icon.classList[0] == "back"){
+                icon.parentNode.style.transform = "rotateY(180deg)";
+            }
+        }, false)
+
+        shield.addEventListener("mouseout", (event)=>{
+            let icon = event.target;
+            if(icon.classList[0] == "front"){
+                icon.parentNode.style.transform = "rotateY(0deg)";
+            }
+        }, false)
+    })
+}
+
+activate();
