be568a7d5e7321926c166ab77e00d00c7d152960
thyezoliveira <thyezoliveira@gmail.com>
2021-04-06 18:01:35 -0300

    update2.0.6
    Interface que guarda o valor do id da janela aberta e devolve quando fecha. Funcionando!

--- js/scripts.js
+++ js/scripts.js
@@ -15,22 +15,25 @@ btnClose.addEventListener("click", shrinkBox);
 
 function growBox(e){
 	this.classList.add('active');
-    activeBox = this.id;
-    this.id = "";
-
-    boxes.forEach(box => {
-        console.log(box);
-        if(box.classList[1] != "active"){
-            box.style.display = "none";
-        }
-    })
-
-	setTimeout(()=>{
-		document.querySelector('#btnClose').classList.add('appear');
-	},500)
+    
+    if(this.id != "null"){
+        activeBox = this.id;
+        this.id = null;
+        boxes.forEach(box => {
+            if(box.classList[1] != "active"){
+                box.style.display = "none";
+            }
+        })
+         
+        setTimeout(()=>{
+            container.removeEventListener("click", containerGridFlexToggle);
+            document.querySelector('#btnClose').classList.add('appear');
+        },500)
+    }
 }
 
 function shrinkBox(e){
+    container.addEventListener("click", containerGridFlexToggle);
     let box = document.querySelector('.box.active');
     box.id = activeBox;
     box.classList.remove('active');
@@ -50,31 +53,29 @@ function containerGridFlexToggle(){
         container.style.display = "grid";
         state = 0;
     }
-
-    console.log(state)
 }
 
 //Function to list all projects and append in a container within cards
 
-// let projectsHolder = document.querySelector(".projects");
-// let allProjects = "";
+let projectsHolder = document.querySelector(".projects");
+let allProjects = "";
 
-// projects.forEach((project, i) => {
-//     let template = `
-//     <article class="projects_article">
-//         <header class="projects_article_header">
-//             <h3>${project.titulo}</h3>
-//         </header>
-//         <img src="${project.imagem}" alt="${project.imagemAlt}" class="projects_article_thumb">
-//         <p class="projects_article_description">${project.descricao}</p>
-//         <div class="projects_article_footer">
-//             <p class="projects_article_footer_date">${project.data}</p>
-//             <a href="${project.link}" target="_blank" id="projectBtn${i}" class="projects_article_footer_a"><img src="./assets/iconLink.svg"
-//                     alt=""></a>
-//         </div>
-//     </article>
-//     `;
-//     allProjects += template;
-// })
+projects.forEach((project, i) => {
+    let template = `
+    <article class="projects_article">
+        <header class="projects_article_header">
+            <h3>${project.titulo}</h3>
+        </header>
+        <img src="${project.imagem}" alt="${project.imagemAlt}" class="projects_article_thumb">
+        <p class="projects_article_description">${project.descricao}</p>
+        <div class="projects_article_footer">
+            <p class="projects_article_footer_date">${project.data}</p>
+            <a href="${project.link}" target="_blank" id="projectBtn${i}" class="projects_article_footer_a"><img src="./assets/iconLink.svg"
+                    alt=""></a>
+        </div>
+    </article>
+    `;
+    allProjects += template;
+})
 
-// projectsHolder.innerHTML = allProjects;
+projectsHolder.innerHTML = allProjects;
