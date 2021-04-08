e512ac8fde54c30515a390ddb4122288e48f7565
thyezoliveira <thyezoliveira@gmail.com>
2021-04-07 23:33:00 -0300

    Update projects content.

--- js/content.js
+++ js/content.js
@@ -12,7 +12,15 @@ export const projects = [
         imagem: "https://github.com/thyezoliveira/Treinamento_Clone_CH/raw/main/assets/meuApp_wImages.png",
         imagemAlt: "Interface do aplicativo club house.",
         descricao: "Um clone da interface do aplicativo club house, usando TailwindCSS.",
-        data: "15/02/2021",
+        data: "24/02/2021",
         link: "https://github.com/thyezoliveira/Treinamento_Clone_CH"
+    },
+    {
+        titulo: "To-do",
+        imagem: "https://github.com/thyezoliveira/projetoDeEstudo_listaDeTarefas/raw/main/assets/captura_tablet.png",
+        imagemAlt: "Interface do aplicativo To-do.",
+        descricao: "O To-do, é um app de lista de tarefas com objetivo simples de adicionar e remover tarefas de uma lista.",
+        data: "05/03/2021",
+        link: "https://github.com/thyezoliveira/projetoDeEstudo_listaDeTarefas"
     }
 ]
